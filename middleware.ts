bc18030f6a26f9d1e73e65f49c00687d7f407692
Nishan Singh <nishan456singh@gmail.com>
2024-05-27 14:39:31 -0700

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,13 @@
-import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
- 
+import { NextResponse } from "next/server";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs/server";
+
 export default authMiddleware({
-  afterAuth(auth, req) {
+  afterAuth(auth, req, evt) {
     // Handle users who aren't authenticated
-        if (auth.userId && auth.isPublicRoute) {
-          return redirectToSignIn({ returnBackUrl: req.url });
-        }
-      },
+    if (auth.userId && auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+  },
       publicRoutes: [
         '/',
         '/events/:id',

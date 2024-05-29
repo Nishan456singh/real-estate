c7eb271b1b982c60e76ed06bc996a71b99c8a457
Nishan Singh <nishan456singh@gmail.com>
2024-05-28 17:27:26 -0700

    aura

--- components/shared/TestimonialCards.tsx
+++ components/shared/TestimonialCards.tsx
@@ -5,10 +5,10 @@ import { SparklesCore } from "../ui/sparkles";
 function SparklesPreview() {
   return (
     <div className=" bg-black flex flex-col items-center justify-center">
-      <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20 mt-10">
+      <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20 mt-4">
         Aura
       </h1>
-      <div className="w-[40rem] h-40 relative">
+      <div className="w-[40rem] h-10 relative">
         {/* Gradients */}
         <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
         <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />

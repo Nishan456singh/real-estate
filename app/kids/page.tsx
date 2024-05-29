13ff861d086d885101afb6000fb79e176b3963e7
Nishan Singh <nishan456singh@gmail.com>
2024-05-29 12:35:27 -0700

    y

--- app/kids/page.tsx
+++ app/kids/page.tsx
@@ -4,7 +4,6 @@ import Header from '@/components/shared/Header'
 import { getAllEvents } from '@/lib/actions/event.actions';
 import { SearchParamProps } from '@/types';
 import React from 'react'
-import { getEventById, getRelatedEventsByCategory } from '@/lib/actions/event.actions';
 
 async function KidsPage({ searchParams }: SearchParamProps) {
   const page = Number(searchParams?.page) || 1;
@@ -15,20 +14,13 @@ async function KidsPage({ searchParams }: SearchParamProps) {
     query: searchText,
     limit: 6,
     page,
-    category:'kids'
+    category: 'kids'
   });
-    const event = await getEventById(category);
-
-    const relatedEvents = await getRelatedEventsByCategory({
-        categoryId: event.category._id,
-        eventId: event._id,
-        page: searchParams.page as string,
-    })
   return (
-  <>
-  <Header />
-    <section className="px-4 sm:px-6 mt-6 mb-6 lg:px-8"> 
-      <Collection
+    <>
+      <Header />
+      <section className="px-4 sm:px-6 mt-6 mb-6 lg:px-8">
+        <Collection
           data={events?.data}
           emptyTitle="No selection available"
           emptyStateSubtext="Come back later"
@@ -36,21 +28,9 @@ async function KidsPage({ searchParams }: SearchParamProps) {
           limit={6}
           page={page}
           totalPages={events?.totalPages} />
-    </section>
-    <section className='wrapper my-8 flex flex-col gap-8 mg:gap12'>
-                <h2 className='h2-bold text-gray-600'>You May Also Like</h2>
-
-                <Collection
-                    data={relatedEvents?.data}
-                    emptyTitle="No Selection Available"
-                    emptyStateSubtext="Come back later"
-                    collectionType="All_Events"
-                    limit={3}
-                    page={searchParams.page as string}
-                    totalPages={relatedEvents?.totalPages} />
-            </section>
-    <Footer />
-  </>
+      </section>
+      <Footer />
+    </>
   )
 }
 

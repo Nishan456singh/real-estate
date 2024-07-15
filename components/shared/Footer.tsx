5a1f43d196645c6f404795a8846a13eea17107bf
Nishan Singh <nishan456singh@gmail.com>
2024-07-14 20:25:09 -0700

    ye

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -3,8 +3,8 @@ import Link from "next/link"
 
 function Footer() {
   return (
-    <footer className="bg-black text-gray-400 py-12">
-      <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-6 lg:px-8">
+    <footer className="bg-black text-gray-400 py-10">
+      <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-2 sm:px-3 lg:px-4">
         <div>
           {/* <Image src='/assets/images/logo.png'
           alt="logo"
